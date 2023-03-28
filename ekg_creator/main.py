fa92d09da3c7ddd483cd06e8e17e194109cf074d
Ava-S <ava.swevels@gmail.com>
2023-03-28 09:43:02 +0200

    - Remove unneeded files (from BPM paper) - Change main.py

--- ekg_creator/main.py
+++ ekg_creator/main.py
@@ -4,24 +4,30 @@ from data_managers.interpreters import Interpreter
 from data_managers.semantic_header import SemanticHeader
 from database_managers.EventKnowledgeGraph import EventKnowledgeGraph, DatabaseConnection
 from data_managers.datastructures import ImportedDataStructures
+
+# several steps of import, each can be switch on/off
 from utilities.performance_handling import Performance
+from colorama import Fore
+
 from database_managers import authentication
 
 connection = authentication.connections_map[authentication.Connections.LOCAL]
 
-dataset_name = 'BoxProcess'
+dataset_name = 'BPIC17'
+use_sample = True
 
 query_interpreter = Interpreter("Cypher")
 semantic_header = SemanticHeader.create_semantic_header(dataset_name, query_interpreter)
 perf_path = os.path.join("..", "perf", dataset_name, f"{dataset_name}Performance.csv")
-number_of_steps = 35
+number_of_steps = 100
 
 datastructures = ImportedDataStructures(dataset_name)
 
-# several steps of import, each can be switch on/off
 step_clear_db = True
 step_populate_graph = True
-verbose = False  # print the used queries
+
+use_preloaded_files = False  # if false, read/import files instead
+verbose = False
 
 db_connection = DatabaseConnection(db_name=connection.user, uri=connection.uri, user=connection.user,
                                    password=connection.password, verbose=verbose)
@@ -34,7 +40,8 @@ def create_graph_instance(perf: Performance) -> EventKnowledgeGraph:
     """
 
     return EventKnowledgeGraph(db_connection=db_connection, db_name=connection.user,
-                               event_tables=datastructures, semantic_header=semantic_header, perf=perf)
+                               batch_size=5000, event_tables=datastructures, use_sample=use_sample,
+                               semantic_header=semantic_header, perf=perf)
 
 
 def clear_graph(graph: EventKnowledgeGraph, perf: Performance) -> None:
@@ -51,49 +58,50 @@ def clear_graph(graph: EventKnowledgeGraph, perf: Performance) -> None:
 
 
 def populate_graph(graph: EventKnowledgeGraph, perf: Performance):
-    # region create EKG with event data and its context
-    # import the events, location and activity records from all sublogs in the graph with the corresponding labels
-    graph.import_data()
-    perf.finished_step(log_message=f"(:Event), (:Location) and (:Activity) nodes done")
+    graph.create_static_nodes_and_relations()
 
-    # for each entity, we add the entity nodes to graph and correlate them (if possible) to the corresponding events
+    # import the events from all sublogs in the graph with the corresponding labels
+    graph.import_data()
+    perf.finished_step(log_message=f"(:Event) nodes done")
+
+    # TODO: constraints in semantic header?
+    graph.set_constraints()
+    perf.finished_step(log_message=f"All constraints are set")
+
+    graph.create_log()
+    perf.finished_step(log_message=f"(:Log) nodes and [:HAS] relations done")
+
+    # for each entity, we add the entity nodes to graph and correlate them to the correct events
     graph.create_entities_by_nodes(node_label="Event")
     perf.finished_step(log_message=f"(:Entity) nodes done")
 
     graph.correlate_events_to_entities(node_label="Event")
     perf.finished_step(log_message=f"[:CORR] edges done")
 
-    # create the classes
     graph.create_classes()
     perf.finished_step(log_message=f"(:Class) nodes done")
 
-    # create [:IS]. [:PART_OF] and [:AT] relation
     graph.create_entity_relations_using_nodes()
-    perf.finished_step(log_message=f"[:REL] edges done")
-    # endregion
-
-    # region Infer missing information
-    # rule c, both for preceding load events and succeeding unload events
-    graph.infer_items_propagate_upwards_multiple_levels(entity_type="Box", is_load=True)
-    graph.infer_items_propagate_upwards_multiple_levels(entity_type="Box", is_load=False)
-    graph.create_entity_relations_using_relations(relation_types=["AT_POS"])
-    # rule d
-    graph.infer_items_propagate_downwards_multiple_level_w_batching(entity_type="Box")
-    # rule b
-    graph.infer_items_propagate_downwards_one_level(entity_type="Box")
-
-    # create corr relation between events and batch positions
-    graph.create_entity_relations_using_relations(relation_types=["CORR"])
+    graph.create_entity_relations_using_relations()
     perf.finished_step(log_message=f"[:REL] edges done")
 
-    graph.add_entity_to_event(entity_type="BatchPosition")
+    graph.create_entities_by_relations()
+    perf.finished_step(log_message=f"Reified (:Entity) nodes done")
 
-    # endregion
+    graph.correlate_events_to_reification()
+    perf.finished_step(log_message=f"[:CORR] edges for Reified (:Entity) nodes done")
 
-    # region Complete EKG creation, add DF relations after missing correlations are inferred
     graph.create_df_edges()
     perf.finished_step(log_message=f"[:DF] edges done")
-    # endregion
+
+    graph.delete_parallel_dfs_derived()
+    perf.finished_step(log_message=f"Deleted all duplicate parallel [:DF] edges done")
+
+    graph.merge_duplicate_df()
+    perf.finished_step(log_message=f"Merged duplicate [:DF] edges done")
+
+    # graph.df_class_relations()
+    # perf.finished_step(log_message=f"[:DF_C] edges done")
 
 
 def main() -> None:
@@ -101,6 +109,10 @@ def main() -> None:
     Main function, read all the logs, clear and create the graph, perform checks
     @return: None
     """
+    if use_preloaded_files:
+        print(Fore.RED + '💾 Preloaded files are used!' + Fore.RESET)
+    else:
+        print(Fore.RED + '📝 Importing and creating files' + Fore.RESET)
 
     # performance class to measure performance
     perf = Performance(perf_path, number_of_steps=number_of_steps)
